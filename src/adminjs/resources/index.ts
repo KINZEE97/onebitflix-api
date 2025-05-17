a58022dafae6fb51ece1ca4956bd03ff9cde9b56
Alizon Kingslen <kingslenalizon@icloud.com>
2025-05-16 22:59:34 -0400

    feat: register Category, Course, and Episode resources with custom options in adminJsResources array for admin panel integration

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,8 +1,9 @@
 
 import { ResourceWithOptions } from "adminjs";
-import { Category, Course } from "../../models";
+import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
+import { episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
     {
@@ -13,6 +14,11 @@ export const adminJsResources: ResourceWithOptions[] = [
     {
         resource: Course,
         options: courseResourceOptions
+    },
+
+    {
+        resource: Episode,
+        options: episodeResourceOptions
     }
 
 ]
