d3d064fd2562cf6e94d4637b4214bbc672266655
Alizon Kingslen <kingslenalizon@icloud.com>
2025-05-16 23:22:53 -0400

    feat: added the episodeResourceFeatures for uploading the episodes

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -3,7 +3,7 @@ import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
-import { episodeResourceOptions } from "./episode";
+import { episodeResourceFeatures, episodeResourceOptions } from "./episode";
 
 export const adminJsResources: ResourceWithOptions[] = [
     {
@@ -18,7 +18,8 @@ export const adminJsResources: ResourceWithOptions[] = [
 
     {
         resource: Episode,
-        options: episodeResourceOptions
+        options: episodeResourceOptions,
+        features: episodeResourceFeatures
     }
 
 ]
