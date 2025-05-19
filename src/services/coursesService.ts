c655a2761168667bebed6e4908b4b3951ec95346
Alizon Kingslen <kingslenalizon@icloud.com>
2025-05-19 19:23:12 -0400

    route to get featured courses

--- src/services/coursesService.ts
+++ src/services/coursesService.ts
@@ -10,7 +10,7 @@ export const coursesService = {
                 ["thumbnail_url", "thumbnailUrl"],
             ],
             include: {
-                association: "Episodes",
+                association: "episodes",
                 attributes: [
                     "id",
                     "name",
@@ -27,4 +27,24 @@ export const coursesService = {
 
         return courseWithEpisodes
     },
+
+    getRandomFeaturedCourses: async () => {
+        const featuredCourses = await Course.findAll({
+            attributes: [
+                "id",
+                "name",
+                "synopsis",
+                ["thumbnail_url", "thumbnailUrl"],
+            ],
+            where: {
+                featured: true
+            },
+
+
+        })
+
+        const randomFeaturedCourses = featuredCourses.sort(() => 0.5 - Math.random())
+
+        return randomFeaturedCourses.slice(0, 3)
+    }
 };
