021576e6373ace756c7325e8f810a3c761400567
Alizon Kingslen <kingslenalizon@icloud.com>
2025-05-20 14:45:16 -0400

    authorization middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { jwtService } from "../services/jwtService";
+import { userService } from "../services/userService";
+import { JwtPayload } from "jsonwebtoken";
+import { UserInstance } from "../models/User";
+
+export interface AuthenticatedRequest extends Request {
+    user?: UserInstance | null;
+}
+
+export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+    const authorizationHeader = req.headers.authorization;
+    if (!authorizationHeader) return res.status(401).json({ message: "Unauthorized, no token found" })
+    const token = authorizationHeader.replace(/Bearer /, "")
+
+    jwtService.verifyToken(token, (err, decoded) => {
+        if (err || typeof decoded === "undefined") {
+            return res.status(401).json({ message: "unauthorized, invalid token" })
+        }
+
+        userService.findByEmail((decoded as JwtPayload).email).then(user => {
+            req.user = user;
+            next();
+        })
+    })
+}
--- src/services/jwtService.ts
+++ src/services/jwtService.ts
@@ -7,5 +7,10 @@ export const jwtService = {
         return jwt.sign(payload, secret, {
             expiresIn: expiration
         })
+    },
+
+    verifyToken: (token: string, callbackfn: jwt.VerifyCallback) => {
+        jwt.verify(token, secret, callbackfn)
+
     }
 }
