4623fe244e2e1c8f0b4304ce090aa6aa884866a7
Alizon Kingslen <kingslenalizon@icloud.com>
2025-05-19 22:12:41 -0400

    user register route

--- src/routes.ts
+++ src/routes.ts
@@ -2,8 +2,11 @@ import express from 'express'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodesController'
+import { authController } from './controllers/authController'
 
 const router = express.Router()
+
+router.post('/auth/register', authController.register)
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
@@ -14,6 +17,8 @@ router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
 
 router.get('/episodes/stream', episodesController.stream)
+
+
 
 
 export { router }
