9fbf43001da9d570bb773ab7f185fb09e1665a99
Alizon Kingslen <kingslenalizon@icloud.com>
2025-05-19 23:06:04 -0400

    login route with jsonwebtoken

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { userService } from "../services/userService";
+import { jwtService } from "../services/jwtService";
 
 
 export const authController = {
@@ -25,6 +26,36 @@ export const authController = {
             if (error instanceof Error) {
                 return res.status(500).json({ message: error.message })
             }
+        }
+    },
+
+    login: async (req: Request, res: Response) => {
+        const { email, password } = req.body
+        try {
+            const user = await userService.findByEmail(email)
+            if (!user) return res.status(404).json({ message: "User not found" })
+
+            user.checkPassword(password, (err, isSame) => {
+                if (err) return res.status(400).json({ message: err.message });
+                if (!isSame) return res.status(401).json({ message: "Invalid password" });
+
+                const payload = {
+                    id: user.id,
+                    firstName: user.firstName,
+                    email: user.email
+                }
+                const token = jwtService.signToken(payload, '7d')
+
+
+                return res.status(200).json({ authenticated: true, ...payload, token })
+
+
+            })
+        } catch (error) {
+            if (error instanceof Error) {
+                return res.status(500).json({ message: error.message })
+            }
+
         }
     }
 }
---
+++ src/services/jwtService.ts
@@ -0,0 +1,11 @@
+import jwt, { Secret } from 'jsonwebtoken'
+
+const secret: Secret = 'your_jwt_secret'
+
+export const jwtService = {
+    signToken: (payload: string | object | Buffer, expiration: string | number) => {
+        return jwt.sign(payload, secret, {
+            expiresIn: expiration
+        })
+    }
+}
