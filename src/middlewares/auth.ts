14f76632f3cb7a57f9d83daba96294503ae691ec
Alizon Kingslen <kingslenalizon@icloud.com>
2025-05-20 15:36:22 -0400

    protecting videos with a middlware for authorization

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -13,14 +13,32 @@ export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextF
     if (!authorizationHeader) return res.status(401).json({ message: "Unauthorized, no token found" })
     const token = authorizationHeader.replace(/Bearer /, "")
 
-    jwtService.verifyToken(token, (err, decoded) => {
+    jwtService.verifyToken(token, async (err, decoded) => {
         if (err || typeof decoded === "undefined") {
             return res.status(401).json({ message: "unauthorized, invalid token" })
         }
 
-        userService.findByEmail((decoded as JwtPayload).email).then(user => {
-            req.user = user;
-            next();
-        })
+        const user = await userService.findByEmail((decoded as JwtPayload).email)
+        req.user = user;
+        next();
     })
+}
+
+
+export async function ensureAuthViaQuery(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+    const { token } = req.query;
+    if (!token) return res.status(401).json({ message: "Unauthorized, no token found" })
+
+    if (typeof token !== "string") return res.status(401).json({ message: "the paramete 'token' must be a string" })
+
+    jwtService.verifyToken(token, async (err, decoded) => {
+        if (err || typeof decoded === "undefined") {
+            return res.status(401).json({ message: "unauthorized, invalid token" })
+        }
+        const user = await userService.findByEmail((decoded as JwtPayload).email)
+        req.user = user;
+        next()
+    })
+
+
 }
