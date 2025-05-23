6f9ba0fa5d1a7c39ab35e15b7c4d1703de05b029
Alizon Kingslen <kingslenalizon@icloud.com>
2025-05-23 19:25:32 -0400

    adding cors

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -3,25 +3,23 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userService";
 
 export const usersController = {
-
-    //GET /users/current 
+    //GET /users/current
 
     show: async (req: AuthenticatedRequest, res: Response) => {
-        const currentUser = req.user!
+        const currentUser = req.user!;
 
         try {
-            return res.json(currentUser)
+            return res.json(currentUser);
         } catch (error) {
             if (error instanceof Error) {
-                return res.status(400).json({ message: error.message })
+                return res.status(400).json({ message: error.message });
             }
         }
     },
     //PUT /users/current
     update: async (req: AuthenticatedRequest, res: Response) => {
-        const { id } = req.user!
-        const { firstName, lastName, phone, email, birth } = req.body
-
+        const { id } = req.user!;
+        const { firstName, lastName, phone, email, birth } = req.body;
 
         try {
             const updatedUser = await userService.update(id, {
@@ -29,13 +27,13 @@ export const usersController = {
                 lastName,
                 phone,
                 email,
-                birth
-            })
+                birth,
+            });
 
-            return res.json(updatedUser)
+            return res.json(updatedUser);
         } catch (err) {
             if (err instanceof Error) {
-                return res.status(400).json({ message: err.message })
+                return res.status(400).json({ message: err.message });
             }
         }
     },
@@ -43,29 +41,27 @@ export const usersController = {
     //PUT /users/current/password
 
     updatePassword: async (req: AuthenticatedRequest, res: Response) => {
-        const user = req.user!
-        const { currentPassword, newPassword } = req.body
+        const user = req.user!;
+        const { currentPassword, newPassword } = req.body;
 
         user.checkPassword(currentPassword, async (error, isSame) => {
-
             try {
-                if (error) return res.status(400).json({ message: error.message })
-                if (!isSame) return res.status(400).json({ message: 'uncorrect password' })
+                if (error)
+                    return res.status(400).json({ message: error.message });
+                if (!isSame)
+                    return res
+                        .status(400)
+                        .json({ message: "uncorrect password" });
 
-                await userService.updatePassword(user.id, newPassword)
-                return res.status(204).send()
+                await userService.updatePassword(user.id, newPassword);
+                return res.status(204).send();
             } catch (error) {
                 if (error instanceof Error) {
-                    return res.status(400).json({ message: error.message })
+                    return res.status(400).json({ message: error.message });
                 }
             }
-
-
-        })
-
-
+        });
     },
-
 
     //GET /users/current/watching
     watching: async (req: AuthenticatedRequest, res: Response) => {
@@ -80,6 +76,4 @@ export const usersController = {
             }
         }
     },
-
-
 };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import cors from 'cors'
 import { sequelize } from './database'
 import { adminJs, adminJsRouter } from './adminjs'
 import { router } from './routes'
 
 const app = express()
+
+app.use(cors())
+
 
 app.use(express.json())
 
