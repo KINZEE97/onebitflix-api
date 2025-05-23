34b857fdd44dd819cb7545baeee4c7052f1bfec9
Alizon Kingslen <kingslenalizon@icloud.com>
2025-05-23 19:20:56 -0400

    updating password

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -40,6 +40,32 @@ export const usersController = {
         }
     },
 
+    //PUT /users/current/password
+
+    updatePassword: async (req: AuthenticatedRequest, res: Response) => {
+        const user = req.user!
+        const { currentPassword, newPassword } = req.body
+
+        user.checkPassword(currentPassword, async (error, isSame) => {
+
+            try {
+                if (error) return res.status(400).json({ message: error.message })
+                if (!isSame) return res.status(400).json({ message: 'uncorrect password' })
+
+                await userService.updatePassword(user.id, newPassword)
+                return res.status(204).send()
+            } catch (error) {
+                if (error instanceof Error) {
+                    return res.status(400).json({ message: error.message })
+                }
+            }
+
+
+        })
+
+
+    },
+
 
     //GET /users/current/watching
     watching: async (req: AuthenticatedRequest, res: Response) => {
